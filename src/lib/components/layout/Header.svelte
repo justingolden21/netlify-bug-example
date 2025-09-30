baabc04e46baecfb770baf66a6ad52252039d3e4
Justin Golden <justingolden21@gmail.com>
2025-09-29 23:15:18 -0700

    Simplify header and layout

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -6,15 +6,8 @@
 	let { navOpen = $bindable() }: Props = $props();
 </script>
 
-<!-- Cover the page in a div with `cursor-none` that only appears when idle
-to hide the cursor when idle -->
-<div id="cursor-cover" class="fixed left-0 top-0 w-full h-full z-30"></div>
-
-<header class="relative w-full print:hidden p-4">
+<header>
 	{#if navOpen}
-		<div
-			class="inline z-30 absolute w-10 ml-16
-		{'md:hidden'}"
-		></div>
+		hello
 	{/if}
 </header>
