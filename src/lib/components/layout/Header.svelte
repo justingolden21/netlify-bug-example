ce8e660d666f7493057187980e906ae34d958f32
Justin Golden <justingolden21@gmail.com>
2025-09-29 23:03:05 -0700

    Remove settings from header

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -1,56 +1,20 @@
 <script lang="ts">
-
-	
-	import { settings } from '$lib/stores/settings';
-
 	interface Props {
 		navOpen: boolean;
 	}
 
 	let { navOpen = $bindable() }: Props = $props();
-
-
-
-	
 </script>
-
 
 <!-- Cover the page in a div with `cursor-none` that only appears when idle
 to hide the cursor when idle -->
-<div
-	id="cursor-cover"
-	class="fixed left-0 top-0 w-full h-full z-30 ">
-</div>
+<div id="cursor-cover" class="fixed left-0 top-0 w-full h-full z-30"></div>
 
 <header class="relative w-full print:hidden p-4">
-
-	{#if navOpen && !$settings.smallerMenu}
+	{#if navOpen}
 		<div
 			class="inline z-30 absolute w-10 ml-16
-		{$settings.alwaysCollapseMenu || true ? '' : 'md:hidden'}">
-			
-		</div>
+		{'md:hidden'}"
+		></div>
 	{/if}
-
-	<!-- Hide top buttons on short screens and fade out if idle -
-		do this here instead of `<header>` because otherwise we hide the nav controls -->
-	<div
-		class="flex items-start gap-2 short:hidden short:h-0 transition-opacity ">
-		<div
-			class="isolate z-10 inline-block ml-16 {$settings.alwaysCollapseMenu || true
-				? ''
-				: 'md:ml-0'}">
-			
-
-			<!-- only show toggle display buttons on pages with toggleable displays -->
-			
-
-		
-		</div>
-
-		<span class="grow"></span>
-		
-
-		
-	</div>
 </header>
